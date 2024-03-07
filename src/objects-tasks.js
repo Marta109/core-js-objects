27f2af847f0a417d54d2268091b933e422badba5
Marta109 <marta.hayrapetyan.a@gmail.com>
2024-03-07 16:50:34 +0400

    feat: add makeWord task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -130,9 +130,18 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const result = [];
+  Object.keys(lettersObject).forEach((letter) => {
+    lettersObject[letter].forEach((i) => {
+      result[i] = letter;
+    });
+  });
+  return result.join('');
 }
+
+// makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] });
+// makeWord({ H: [0], e: [1], l: [2, 3, 8], o: [4, 6], W: [5], r: [7], d: [9] });
 
 /**
  * There is a queue for tickets to a popular movie.
